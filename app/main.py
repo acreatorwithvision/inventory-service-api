d4635f4d0e411f8f044496a2c5cc6181367f82c3
acreatorwithvision <fake49403@gmail.com>
2026-02-08 12:41:35 +0530

    implement full CRUD for in-memor inventory

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import List
 
 from app.models import Item, ItemCreate
@@ -27,3 +27,33 @@ def create_item(item: ItemCreate):
 @app.get("/items",response_model=List[Item])
 def list_items():
     return list(inventory_db.values())
+
+@app.get("/items/{item_id}",response_model=Item)
+def get_item(item_id:str):
+    item=inventory_db.get(item_id)
+
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    return item
+
+@app.put("/items/{item_id}",response_model=Item)
+def update_item(item_id:str , updated: ItemCreate):
+    existing=inventory_db.get(item_id)
+
+    if not existing:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    updated_item=Item(id=item_id, **updated.model_dump())
+    inventory_db[item_id]=updated_item
+
+    return updated_item
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: str):
+    if item_id not in inventory_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    del inventory_db[item_id]
+
+    return {"message":"Item deleted Successfully"}
